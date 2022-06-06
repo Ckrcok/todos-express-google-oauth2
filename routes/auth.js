a53aa131b152c50f17bb3c56fc346bea27a3f954
Dor <dorzairi@ymail.com>
2022-06-06 11:37:14 -0400

    deleted some auth stuff to practice

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,6 @@
-var express = require('express');
-var passport = require('passport');
-var GoogleStrategy = require('passport-google-oauth20');
-var db = require('../db');
+// TODO 1 Npm install passport-google-oauth20
 
+// TODO 2 Import passport and keys
 
 // Configure the Facebook strategy for use by Passport.
 //
@@ -11,47 +9,9 @@ var db = require('../db');
 // behalf, along with the user's profile.  The function must invoke `cb`
 // with a user object, which will be set at `req.user` in route handlers after
 // authentication.
-passport.use(new GoogleStrategy({
-  clientID: process.env['GOOGLE_CLIENT_ID'],
-  clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-  callbackURL: '/oauth2/redirect/google',
-  scope: [ 'profile' ],
-  state: true
-},
-function(accessToken, refreshToken, profile, cb) {
-  db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-    'https://accounts.google.com',
-    profile.id
-  ], function(err, row) {
-    if (err) { return cb(err); }
-    if (!row) {
-      db.run('INSERT INTO users (name) VALUES (?)', [
-        profile.displayName
-      ], function(err) {
-        if (err) { return cb(err); }
-        var id = this.lastID;
-        db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-          id,
-          'https://accounts.google.com',
-          profile.id
-        ], function(err) {
-          if (err) { return cb(err); }
-          var user = {
-            id: id,
-            name: profile.displayName
-          };
-          return cb(null, user);
-        });
-      });
-    } else {
-      db.get('SELECT rowid AS id, * FROM users WHERE rowid = ?', [ row.user_id ], function(err, row) {
-        if (err) { return cb(err); }
-        if (!row) { return cb(null, false); }
-        return cb(null, row);
-      });
-    }
-  });
-}));
+
+// TODO 3 Configure the Google strategy for use by Passport.
+
   
 // Configure Passport authenticated session persistence.
 //
@@ -62,20 +22,10 @@ function(accessToken, refreshToken, profile, cb) {
 // from the database when deserializing.  However, due to the fact that this
 // example does not have a database, the complete Facebook profile is serialized
 // and deserialized.
-passport.serializeUser(function(user, cb) {
-  process.nextTick(function() {
-    cb(null, { id: user.id, username: user.username, name: user.name });
-  });
-});
 
-passport.deserializeUser(function(user, cb) {
-  process.nextTick(function() {
-    return cb(null, user);
-  });
-});
+// TODO 4 Configure Passport authenticated session persistence.
 
 
-var router = express.Router();
 
 /* GET /login
  *
@@ -85,9 +35,10 @@ var router = express.Router();
  * user to sign in with Google.  When the user clicks this button, a request
  * will be sent to the `GET /login/federated/accounts.google.com` route.
  */
-router.get('/login', function(req, res, next) {
-  res.render('login');
-});
+
+
+// TODO 5 Configure the GET /login route.
+
 
 /* GET /login/federated/accounts.google.com
  *
@@ -101,7 +52,11 @@ router.get('/login', function(req, res, next) {
  * Once Google has completed their interaction with the user, the user will be
  * redirected back to the app at `GET /oauth2/redirect/accounts.google.com`.
  */
-router.get('/login/federated/google', passport.authenticate('google'));
+
+
+// TODO 6 Configure the GET /login/federated/accounts.google.com route.
+
+
 
 /*
     This route completes the authentication sequence when Google redirects the
@@ -109,18 +64,14 @@ router.get('/login/federated/google', passport.authenticate('google'));
     automatically created and their Google account is linked.  When an existing
     user returns, they are signed in to their linked account.
 */
-router.get('/oauth2/redirect/google', passport.authenticate('google', {
-  successReturnToOrRedirect: '/',
-  failureRedirect: '/login'
-}));
+// TODO 7 Configure the GET /oauth2/redirect/accounts.google.com route.
+
 
 /* POST /logout
  *
  * This route logs the user out.
  */
-router.post('/logout', function(req, res, next) {
-  req.logout();
-  res.redirect('/');
-});
+// TODO 8 Configure the POST /logout route.
+
 
 module.exports = router;
